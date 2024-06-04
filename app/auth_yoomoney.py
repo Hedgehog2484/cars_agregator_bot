e028deb0f0d640697de7044221c7dc36735d7e16
Svyatoslav <hedgehogdevelop@gmail.com>
2024-06-04 18:55:55 +0300

    aboba

--- app/auth_yoomoney.py
+++ app/auth_yoomoney.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
         client_secret=cfg.yoomoney_client_secret.get_secret_value(),
         base_api_url=cfg.yoomoney_base_url,
         redirect_url=cfg.yoomoney_redirect_url,
-        auth_token=""
+        auth_token="",
+        receiver_number="",
+        payment_type=""
     )
     asyncio.run(yw.authorize())
     print("Authorization is ended.")
