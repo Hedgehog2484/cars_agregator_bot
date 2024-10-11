cdd24a12087871bc8cf9b7a527aee034f1ab3b65
Svyatoslav <hedgehogdevelop@gmail.com>
2024-10-11 18:46:33 +0300

    Get to post

--- app/web/webapp.py
+++ app/web/webapp.py
@@ -32,7 +32,7 @@ def start_webapp(db):
     # app.mount("/web/static", StaticFiles(directory="static", html=True), name="static")
 
     app.router.get("/", response_class=FileResponse)(send_page)
-    app.router.get("save_settings")(get_user_settings)
+    app.router.post("/save_settings")(get_user_settings)
     # uvicorn.run(app, host="0.0.0.0", port=8432, ssl_keyfile="./0.0.0.0-key.pem", ssl_certfile="./0.0.0.0.pem")
     uvicorn.run(
         app, host="pepepu.ru", port=443, ssl_keyfile="web/key.pem", ssl_certfile="web/certificate.crt"
