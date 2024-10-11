56400370504af8dcbf23aa933f923a4f446ac8af
Svyatoslav <hedgehogdevelop@gmail.com>
2024-10-11 17:39:17 +0300

    Cert and key files path change

--- app/web/webapp.py
+++ app/web/webapp.py
@@ -35,5 +35,5 @@ def start_webapp(db):
     app.router.get("save_settings")(get_user_settings)
     # uvicorn.run(app, host="0.0.0.0", port=8432, ssl_keyfile="./0.0.0.0-key.pem", ssl_certfile="./0.0.0.0.pem")
     uvicorn.run(
-        app, host="pepepu.ru", port=443, ssl_keyfile="./key.pem", ssl_certfile="./certificate.crt"
+        app, host="pepepu.ru", port=443, ssl_keyfile="key.pem", ssl_certfile="certificate.crt"
     )
